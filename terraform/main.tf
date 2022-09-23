eab04b323b0620c4802ceb4047e1e21f38950a18
fabiomaster01 <fabio.master.job@gmail.com>
2022-09-23 22:44:18 +0100

    terraform test

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9" 
+
+   backend "s3" {
+       bucket = "fm-terratest1"
+       key    = "terraform-state/"
+       region = "east-us-1"
+   }
+
+}
+
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-1"
+}
+
+esource "aws_s3_bucket" "s3Bucket" {
+     bucket = "fm-terratest1"
+     acl       = "public-read"
+
+     policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::fm-terratest1/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+   website {
+       index_document = "index.html"
+   }
+}
